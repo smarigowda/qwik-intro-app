b36809cceef6c490f967c47a10ca83e7412ea23f
Santosh Marigowda <santosh.marigowda@abcam.com>
2023-05-31 19:09:41 +0100

    passing props

--- src/routes/page1/index.tsx
+++ src/routes/page1/index.tsx
@@ -1,17 +1,23 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, useSignal } from "@builder.io/qwik";
+
+import { Projector } from "./projector";
 
 export default component$(() => {
+  const messageSignal = useSignal("");
   return (
     <div>
       This is Page 1
       <hr />
       <input
         type="text"
-        placeholder="Type, listening to keydown event)"
-        onKeyDown$={(event) => console.log(event.key)}
+        value={messageSignal.value}
+        placeholder="Type, listening to OnInput event..."
+        onInput$={(event) =>
+          (messageSignal.value = (event.target as HTMLInputElement).value)
+        }
       />
       <hr />
-      <div>You typed: </div>
+      <Projector message={messageSignal.value} />
     </div>
   );
 });
---
+++ src/routes/page1/projector.tsx
@@ -0,0 +1,9 @@
+import { component$ } from "@builder.io/qwik";
+
+export interface ProjectorProps {
+    message: string;
+}
+
+export const Projector = component$((props: ProjectorProps) => {
+  return <div>You typed...{props.message}</div>;
+});
